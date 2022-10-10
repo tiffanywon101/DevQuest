45ee5c4f69cb496ba086fee62cdbe2ca899b65cc
tiffanywon101 <tiffanywon101@naver.com>
2022-10-10 15:42:19 +0900

    원다빈_10.06_DevQuest
    저번주, 게임을 플레이 했을 때, 캐릭터가 그라운드를 뚫고 내려가 사라진 부분은 고쳤으나, 플레이를 누른 순간에 사라졌다 총알 슈팅을 하기 위해 마우스를 누른 순간에 캐릭터가 등장하며 65번째 줄에서 계속 instance not set라는 오류가 뜹니다.ㅠㅠ

--- Assets/Scripts/ClientCharacterMovement.cs
+++ Assets/Scripts/ClientCharacterMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,60 +8,125 @@ public class ClientCharacterMovement : MonoBehaviour
 {
     private InputAction _movePosition;
     private InputAction _moveAction;
+    private InputAction _jumpAction;
+
     private bool _move = false;
     private Vector3 _targetPos;
-    private float _distanceCheck = 0.1f;
-    [SerializeField] // visible in editor
-    private float _walkSpeed = 1.0f;
+    private float _distanceCheck = 0.10f;
 
+    [SerializeField]
+    private float _walkSpeed = 1.0f;
+    private Animator _anim;
+    private CharacterController _controller;
+
+    [SerializeField]
+    private bool _grounded = true;
     void Start()
     {
-        //Player Input setup
-        _targetPos = transform.position;
         var playerInput = GetComponent<PlayerInput>();
-        // get actions from the Input Action bound to Player Input.
         _movePosition = playerInput.actions["LocoTarget"];
         _moveAction = playerInput.actions["MoveTarget"];
-        
+        _jumpAction = playerInput.actions["Jump"];
+        _targetPos = transform.position;
+        _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
-    //we will check for user input every frame
     private void Update()
     {
         ProcessInput();
-        _move = MoveToPosition(_targetPos);
-    }
-    private bool MoveToPosition(Vector3 _targetPos)
-    {
-        if (Vector3.Magnitude(transform.position - _targetPos) < _distanceCheck)
+        GroundCheck();
+        bool moveResult = MoveToPosition(_targetPos);
+        if (_move != moveResult)
         {
-            // arrived. cancel move.
-            Debug.Log("arrived! stop moving!");
-            return false;
+            _move = moveResult;
+            _anim.SetBool("bWalk", _move);
         }
-        // should move towards target
-        transform.position = Vector3.MoveTowards(transform.position, _targetPos, Time.deltaTime * _walkSpeed);
-        transform.LookAt(_targetPos, Vector3.up);
-        return true;
+
+        _controller.Move(_horizontalVelocity * Time.deltaTime
+            + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+        // gravity
+        if (!_grounded)
+        {
+            _verticalVelocity += _gravity * Time.deltaTime;
+        }
+
+        if (_grounded && _verticalVelocity < 0.0f)
+        {
+            //grounded
+            _verticalVelocity = -0f;
+        }
     }
 
-    // processes user input
     private void ProcessInput()
     {
         // RMB pressed
-        if (_moveAction.triggered)
+        if (_moveAction.triggered && _grounded)
         {
             // mouse position
             Vector2 screenPos = _movePosition.ReadValue<Vector2>();
-            // raycast to the ground, to get the position on the ground to move our character to.
             var ray = Camera.main.ScreenPointToRay(screenPos);
             if (Physics.Raycast(ray, out RaycastHit hit, 500.0f, LayerMask.GetMask("Ground")))
             {
-                //Let's just debug the input for now.
                 _targetPos = hit.point;
-                //Debug.Log(hit.point);
             }
         }
+        if (_jumpAction.triggered && _grounded)
+        {
+            Jump();
+        }
+
+    }
+
+
+
+    private void Jump()
+    {
+        _grounded = false;
+        _verticalVelocity = Mathf.Sqrt(_jumpHeight * -2.0f * _gravity);
+    }
+
+    [SerializeField]
+    private float _gravity = -9.8f;
+    [SerializeField]
+    private float _jumpHeight = 1.0f;
+    [SerializeField]
+    private Transform _footTransform;
+    private float _groundedRadius = 0.3f;
+
+    private float _verticalVelocity = 0.0f;
+    private Vector3 _horizontalVelocity = Vector3.zero;
+
+    private void GroundCheck()
+    {
+        // set sphere position, with offset
+        Vector3 spherePosition = _footTransform.position;
+        _grounded = Physics.CheckSphere(spherePosition, _groundedRadius, LayerMask.GetMask("Ground"), QueryTriggerInteraction.Ignore);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_grounded) Gizmos.color = Color.green;
+        else Gizmos.color = Color.red;
+        Vector3 spherePosition = _footTransform.position;
+        Gizmos.DrawSphere(spherePosition, _groundedRadius);
+    }
+    private bool MoveToPosition(Vector3 _targetPos)
+    {
+        Vector3 groundTargetPos = new Vector3(_targetPos.x, 0.0f, _targetPos.z);
+        Vector3 groundPos = new Vector3(transform.position.x, 0.0f, transform.position.z);
+        if (Vector3.Magnitude(groundPos - groundTargetPos) < _distanceCheck)
+        {
+            // arrived. cancel move.
+            _horizontalVelocity = Vector3.zero;
+            return false;
+        }
+        // should move towards target
+        //transform.position = Vector3.MoveTowards(transform.position, _targetPos, Time.deltaTime * _walkSpeed);
+        _horizontalVelocity = (groundTargetPos - groundPos).normalized * _walkSpeed;
+        transform.LookAt(transform.position + _horizontalVelocity, Vector3.up);
+        return true;
     }
 
 }
